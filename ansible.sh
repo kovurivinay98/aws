6e9a873b909819ddd6671c9242eb1a1cabe89f63
K.Vinay Reddy <vinayreddykovuri@gmail.com>
2025-11-11 14:50:58 +0530

    ansible installation

---
+++ ansible.sh
@@ -0,0 +1,19 @@
+userid=$(id -u)
+if [ $userid -eq 0]
+then
+    dnf list installed ansible
+        if [ $? -ne 0 ]
+        then
+            dnf install ansible -y
+            if [ $? -eq 0 ]
+            then
+                echo "Installation Finished"
+            else
+                echo "Installation Failed"
+            fi
+        else
+            echo " Already Installed"
+        fi
+else
+    echo " Not a Root User"
+fi
